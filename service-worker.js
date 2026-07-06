7d682a0e1c9182d8b1d11820054ca0428452fb5f
monitor0111-netizen <monitor0111@gmail.com>
2026-07-07 02:27:48 +0500

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ohotnik-cache-v1';
+const CACHE_NAME = 'ohotnik-cache-v3';
 const APP_SHELL = [
   './',
   './index.html',
@@ -48,3 +48,4 @@ self.addEventListener('fetch', (event) => {
     })
   );
 });
+
